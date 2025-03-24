2ff58c90c61097a8e20ee7f3b0a96e0fefdb4fc0
Ilyosbek <ilyosbekibroximov23z@gmail.com>
2025-03-24 23:10:38 +0500

    server.js

--- server.js
+++ server.js
@@ -1,13 +1,4 @@
 const express = require("express");
-<<<<<<< HEAD
-require("dotenv").config();
-const app = express();
-app.use(express.json());
-const PORT = process.env.DB_PORT || 3001;
-
-app.listen(PORT, () => {
-    console.log(`Server started has been on PORT: ${PORT}`);
-=======
 const { connectDb } = require("./config/db");
 
 const app = express();
@@ -20,5 +11,4 @@ connectDb();
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server started has been on PORT: ${PORT}`)
->>>>>>> Xusniddin
 });
