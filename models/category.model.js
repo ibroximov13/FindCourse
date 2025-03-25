6850451f89a3cbd386821f6a735db0225666f463
Xamel10n-tech <xamel10n.xs@gmail.com>
2025-03-25 06:35:24 +0500

    package removed

---
+++ models/category.model.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/db");
+
+const Category = sequelize.define("Category", {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    image: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
+
+module.exports = Category;
